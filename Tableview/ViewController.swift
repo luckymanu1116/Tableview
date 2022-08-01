54786fb2bd8d2d2de630acdbb222232e7cc9fff3
Manoj Reddy <luckymanoj1116@gmail.com>
2022-08-01 15:25:51 -0400

    Initial Commit

---
+++ Tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tableview
+//
+//  Created by hari krishna Reddy Regalla on 2022-08-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
